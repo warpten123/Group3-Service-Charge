dc8d49b06f8d21f6551ca6eb4f7c892e409263a1
Paul Joshua Premacio <pauljoshuapremacio@gmail.com>
2022-11-10 22:37:36 +0800

    created ui for landing page (subject to change)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { CreateTicketComponent } from './create-ticket/create-ticket.component';
+import { UpdateTicketComponent } from './update-ticket/update-ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserDashboardComponent,
+    CreateTicketComponent,
+    UpdateTicketComponent
   ],
   imports: [
     BrowserModule,
